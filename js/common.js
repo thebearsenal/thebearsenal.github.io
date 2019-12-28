9af9ddddfab8d0f9f9de5c59305d9bce8179759f
Your Name <you@example.com>
2019-12-28 14:37:45 +0530

    layout udate

--- js/common.js
+++ js/common.js
@@ -10,28 +10,12 @@ function generate_template(){
     loadHead('<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>')
     loadHead('<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>')
     loadHead('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>')
-    loadHead('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-33383432-3"></script>')
-    loadHead('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>')
     loadHead('<link rel="icon" href="../img/logo.png">')
     loadHead('<style>body {font-family: "Times New Roman", Times, serif;}</style>')
-    load_ads()
 }
 function loadHead(data)
 {    
     document.getElementsByTagName('head')[0].innerHTML += data
-}
-function load_ads()
-{    
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-33383432-3');
-
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-3600373850128255",
-              enable_page_level_ads: true
-     });
 }
 
 
