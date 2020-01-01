32a1ec25ea469e93e1ca8db87b91094e4936eaef
Your Name <you@example.com>
2020-01-02 01:37:30 +0530

    index

--- js/elite_list.js
+++ js/elite_list.js
@@ -2,8 +2,8 @@ function generate_elite_list(commands_list){
     document.getElementById("elite_list").innerHTML  = ""
     for (let each of commands_list){
         document.getElementById("elite_list").innerHTML+= "<div class=\"card\"><div class=\"card-header  bg-primary text-white\">"+
-            "Topic: "+each.Topic+"</div><div class=\"card-body\">"+"Command: "+each.Command+
-                "</div><div class=\"card-footer  bg-dark text-white\">"+"Comment: "+each.Comment+
+            "Topic: "+each.Topic+"</div><div class=\"card-body\">"+"\""+each.Command+
+                " \"</div><div class=\"card-footer  bg-dark text-white\">"+"Comment: "+each.Comment+
                     "</div></div><br>"
     }
 }
