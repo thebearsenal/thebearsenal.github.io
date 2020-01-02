0d3a6fdc7d8c602a9b05bf7b115af991de50ee95
Your Name <you@example.com>
2020-01-02 14:27:25 +0530

    index

--- js/elite_list.js
+++ js/elite_list.js
@@ -6,4 +6,14 @@ function generate_elite_list(commands_list){
                 " \"</div><div class=\"card-footer  bg-dark text-white\">"+"Comment: "+each.Comment+
                     "</div></div><br>"
     }
+    sidebar_elite_list()
+}
+function sidebar_elite_list(){
+    document.getElementById("sidebar_list").innerHTML += '<br><h4><a href="https://www.thebearsenal.com/apps/The-Elite-List-Home" >The Elite List</a></h4>'+
+    '<hr style="background-color:#259fea">'+
+    '<ul id="sidebar_list" class="list-group">'+
+    '<li><a href="https://www.thebearsenal.com/apps/SQL-Commands-List">SQL Commands</a></li>'+
+    '<li><a href="https://www.thebearsenal.com/apps/Frequent-Linux-Commands-List">Linux Commands</a></li>'+
+    '</ul>'
+     
 }
